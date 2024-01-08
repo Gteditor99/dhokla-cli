5468c2b4e55811317314cf1fb10c229aaf3a12f4
editor99 <yoonjaekim2010@gmail.com>
2024-01-08 20:47:05 +0900

    feat: proper GET requests implemented

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 )
@@ -58,10 +57,40 @@ type Directory struct {
 // 	return &dir, nil
 // }
 
+func getRootDirectory() (map[string]File, error) {
+	// Retrieves the root directory of dhokla.net (note the lack of an ID in the request)
+	jsonData, err := http.Get("https://dhokla.net/api/d/")
+	if err != nil {
+		return nil, fmt.Errorf("error occurred during request: %v", err)
+	}
+
+	// The root directory is special, since it is used to categorize media based on type.
+	// movies, tv shows, anime, etc.
+	defer jsonData.Body.Close()
+
+	body, err := io.ReadAll(jsonData.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred while reading response body: %v", err)
+	}
+
+	// Unmarshal the JSON data into a Directory struct
+	var dir Directory
+	err = json.Unmarshal(body, &dir)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred while unmarshalling JSON data: %v", err)
+	}
+	// fileMap impl for id referencing
+	fileMap := make(map[string]File)
+
+	for _, file := range dir.Data.Files {
+		fileMap[file.Id] = file
+	}
+	return fileMap, nil
+}
+
 func getDirectory(id string) (map[string]File, error) {
-	// Make a request to retrieve the directory with the given ID
-	// and store the response in jsonData
-	jsonData, err := http.Get("https://dhokla.net/api/directories/" + id)
+	/* Makes a GET request to retrieve specific directories using id. */
+	jsonData, err := http.Get("https://dhokla.net/api/d/" + id)
 	if err != nil {
 		return nil, fmt.Errorf("error occurred during request: %v", err)
 	}
@@ -88,10 +117,13 @@ func getDirectory(id string) (map[string]File, error) {
 	return fileMap, nil
 }
 
-// curl POST "https://dhokla.net/files/b5c7a659-f63c-426e-9f7b-74650ad8a943" >> output.txt
+
+// keep-alive connection for parallel downloads
 func downloadFile(f File) error {
 	fmt.Printf("Started downloading %v, ID: %v, size: %v\n", f.Name, f.Id, f.Size)
-	URL := "https.dhokla.net/files/" + f.Id
+	URL := "https.dhokla.net/f/" + f.Id
+
+	
 
 	// check if URL is up
 	fileData, err := http.Get(URL)
@@ -103,6 +135,12 @@ func downloadFile(f File) error {
 			f.Size,
 		)
 	}
+	// keep-alive connection for parallel downloads
+	req, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Connection", "keep-alive")
 
 	// create file
 	out, err := os.Create(f.Name)
@@ -116,41 +154,68 @@ func downloadFile(f File) error {
 	return err
 }
 
-func main() {
-	// Get the directory with the given ID
-	fmt.Printf("dhokla-cli: Enter directory ID:")
-	fmt.Scan()
-	var dirID string
-	fmt.Scanln(&dirID)
 
-	fileMap, err := getDirectory(dirID)
+type SearchResponse struct {
+	Message string `json:"message"`
+	Data    []File `json:"data"`
+}
+
+// search for files/directories by name
+func search(name string) ([]File, error) {
+	jsonData, err := http.Get("https://dhokla.net/api/s/" + name)
 	if err != nil {
-		log.Fatalf("error occurred while getting directory: %v", err)
+		return nil, fmt.Errorf("error occurred during request: %v", err)
 	}
 
-	// Print out the directory information
-	// fmt.Printf("Directory Name: %s\n", dir.Data.Name)
-	// fmt.Printf("Directory ID: %s\n", dir.Data.Id)
-	// fmt.Printf("Is Directory: %v\n", dir.Data.Dir)
-	//  fmt.Printf("Message: %s\n", dir.Message)
-	//  fmt.Printf("Parent Directory ID: %s\n", dir.Data.Parent)
-	//  fmt.Printf("Modification Time: %s\n", dir.Data.Mtime)
+	defer jsonData.Body.Close()
 
-	for _, file := range fileMap {
-		fmt.Printf(
-			"Name: %s, ID: %s, Size: %d, Modification Time: %s\n",
-			file.Name,
-			file.Id,
-			file.Size,
-			file.Mtime,
-		)
-	}
-	file, ok := fileMap["96a6d6e2-583b-4c08-9598-d2849c354856"]
-	if ok {
-		fmt.Printf("%v\n", file.Id)
-	} else {
-		fmt.Println("No file found with the given ID")
+	body, err := io.ReadAll(jsonData.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred while reading response body: %v", err)
 	}
 
-	// downloadFile(fileMap["96a6d6e2-583b-4c08-9598-d2849c354856"])
+	// Unmarshal the JSON data into a SearchResponse struct
+	var response SearchResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, fmt.Errorf("error occurred while unmarshalling JSON data: %v", err)
+	}
+
+	return response.Data, nil
+}
+
+func main() {
+	// [TEST] Print out the files in the root directory
+	rootFileDirectories, err := getRootDirectory()
+	if err != nil {
+		fmt.Printf("error occurred while getting root directory: %v", err)
+	}
+	for _, file := range rootFileDirectories {
+		fmt.Printf(file.Id + " " + file.Name + "\n")
+	}
+
+	// [TEST] Print out the files in the directory with ID d0404f70-93ff-4059-984f-9b988941f955
+	dir, err := getDirectory("d0404f70-93ff-4059-984f-9b988941f955")
+	if err != nil {
+		fmt.Printf("error occurred while getting directory: %v", err)
+	}
+	for _, file := range dir {
+		fmt.Printf(file.Id + " " + file.Name + "\n")
+	}
+
+	// [TEST] search for files/directories by name
+	var searchQuery string
+	fmt.Printf("Enter search query: ")
+	_, err = fmt.Scanln(&searchQuery)
+	if err != nil {
+		fmt.Printf("error occurred while scanning input: %v", err)
+	}
+
+	searchResults, err := search("'" + searchQuery + "'")
+	if err != nil {
+		fmt.Printf("error occurred while searching: %v", err)
+	}
+	for _, file := range searchResults {
+		fmt.Printf(file.Id + " " + file.Name + "\n")
+	}
 }
